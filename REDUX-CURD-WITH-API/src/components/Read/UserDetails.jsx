0bd9adfb9c958ea02c20ad09da7a9f7945493d47
Harshal.Ogale-at-447515355797 <harshal.ogale@flentas.com>
2024-07-14 18:33:53 +0530

    started get

--- REDUX-CURD-WITH-API/src/components/Read/UserDetails.jsx
+++ REDUX-CURD-WITH-API/src/components/Read/UserDetails.jsx
@@ -1,18 +1,29 @@
+/** @format */
+
+import { useDispatch, useSelector } from 'react-redux';
+import { showUser } from '../../features/userDetailsSlice';
+import { useEffect } from 'react';
 
 const UserDetails = () => {
+  const dispatch = useDispatch();
+
+  const allData = useSelector((state) => state.userDetails);
+  console.log(allData);
+
+  useEffect(() => {
+    console.log('UserDetails useEffect');
+    dispatch(showUser());
+  });
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-4">
-      <div className="font-bold text-xl mb-2">{}</div>
-      <p className="text-gray-700 text-base">{}</p>
-      <div className="flex justify-end mt-4">
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline"
-        >
+    <div className='max-w-sm rounded overflow-hidden shadow-lg bg-white p-6 m-4'>
+      <div className='font-bold text-xl mb-2'>{}</div>
+      <p className='text-gray-700 text-base'>{}</p>
+      <div className='flex justify-end mt-4'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 focus:outline-none focus:shadow-outline'>
           Edit
         </button>
-        <button
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
+        <button className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>
           Delete
         </button>
       </div>
--- REDUX-CURD-WITH-API/src/features/userDetailsSlice.js
+++ REDUX-CURD-WITH-API/src/features/userDetailsSlice.js
@@ -1,27 +1,58 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+/** @format */
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // Create action
-export const createUser = createAsyncThunk("createUser", async (data, { rejectWithValue }) => {
-  try {
-    const response = await fetch("https://668d679a099db4c579f2db14.mockapi.io/crud", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(data)
-    });
+export const createUser = createAsyncThunk(
+  'createUser',
+  async (data, { rejectWithValue }) => {
+    console.log('createUser called');
+    try {
+      const response = await fetch(
+        'https://668d679a099db4c579f2db14.mockapi.io/crud',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    return rejectWithValue(error);
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
   }
-});
+);
+
+//read action
+
+export const showUser = createAsyncThunk(
+  'showUser',
+  async ({ rejectWithValue }) => {
+    console.log('showUser called');
+
+    const response = await fetch(
+      'https://668d679a099db4c579f2db14.mockapi.io/crud'
+    );
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+// =============================================================================
 
 const initialState = {
   users: [],
   loading: false,
-  error: null
+  error: null,
 };
 
 export const userDetail = createSlice({
@@ -41,7 +72,20 @@ export const userDetail = createSlice({
         state.loading = false;
         state.error = action.payload;
       });
-  }
+
+    builder
+      .addCase(showUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(showUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(showUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export default userDetail.reducer;
